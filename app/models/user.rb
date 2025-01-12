4d22d658172b852598fd70001099c329db533d4c
giljr <giljr.2009@gmail.com>
2025-01-12 19:10:52 -0400

    user registrations actions

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# email:string
+# password:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+
+  validates :password, presence: true, length: { minimum: 6 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+
   root to: "main#index"
 end
